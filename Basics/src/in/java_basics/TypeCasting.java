7a4b2df807b185bdbfbbc4719335dcd185f58483
Admin <ananyaarthi2002@gmail.com>
2024-07-19 11:06:20 +0530

    Learnt about TypeCasting

---
+++ Basics/src/in/java_basics/TypeCasting.java
@@ -0,0 +1,20 @@
+package in.java_basics;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+//implicit type casting
+		byte a=100;
+		int b=a;
+		System.out.println(a+" "+b);
+		
+		
+// explicit type casting
+		int x=20;
+		byte y=(byte)x;
+		System.out.println(x+" "+y);
+		
+	}
+
+}
+
