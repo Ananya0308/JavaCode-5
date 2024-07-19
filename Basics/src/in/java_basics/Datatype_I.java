0582488af6ea07f6ee00977e278c0bf3834fc1b5
Admin <ananyaarthi2002@gmail.com>
2024-07-19 10:57:58 +0530

    learnt about datatype

---
+++ Basics/src/in/java_basics/Datatype_I.java
@@ -0,0 +1,31 @@
+package in.java_basics;
+
+public class Datatype_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//size of data types
+		System.out.println("byte size:"+Byte.SIZE+"bits");
+System.out.println(" short size:"+Short.SIZE+"bits");
+System.out.println(" int size:"+Integer.SIZE+"bits");
+System.out.println(" long size:"+Long.SIZE+"bits");
+System.out.println(" float size:"+Float.SIZE+"bits");
+System.out.println(" double size:"+Double.SIZE+"bits");
+System.out.println(" char size:"+Character.SIZE+"bits");
+
+
+// range of data types
+System.out.println(" byte minrange=:"+Byte.MAX_VALUE+"byte maxrange"+Byte.MIN_VALUE);
+System.out.println(" short minsize:"+Short.MAX_VALUE+"shortmaxrange"+Short.MIN_VALUE);
+System.out.println(" int minsize:"+Integer.MAX_VALUE+"int max range"+Integer.MIN_VALUE);
+System.out.println(" long size:"+Long.MAX_VALUE+"bits"+Long.MIN_VALUE+"bits");
+System.out.println(" float size:"+Float.MAX_VALUE+"bits"+Float.MIN_VALUE+"bits");
+System.out.println(" double size:"+Double.MAX_VALUE+"bits"+Double.MIN_VALUE+"bits");
+
+
+
+
+	}
+
+}
